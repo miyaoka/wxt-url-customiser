71e40e249fcb6b72d16d3396e961978d9cf4c2c3
Masaya Kazama <miyaokamiyao@gmail.com>
2024-09-15 02:30:29 +0000

    add vue

---
+++ src/entrypoints/popup/App.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+const currentUrl = ref<string | undefined>(undefined);
+
+browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
+  currentUrl.value = tabs[0].url;
+});
+</script>
+
+<template>
+  <main>
+    <p>url: {{ currentUrl }}</p>
+    <!-- <p>params: {{ params }}</p> -->
+  </main>
+</template>
+
+<style scoped>
+main {
+  padding: 20px;
+  font-size: 16px;
+}
+</style>
