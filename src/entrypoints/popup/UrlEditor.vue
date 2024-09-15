8bc1991f2b34775d52dc28114f3fc84369255ac8
Masaya Kazama <miyaokamiyao@gmail.com>
2024-09-15 23:53:29 +0000

    WIP

--- src/entrypoints/popup/UrlEditor.vue
+++ src/entrypoints/popup/UrlEditor.vue
@@ -9,9 +9,15 @@ interface Param {
   value: string;
 }
 
+interface PathItem {
+  id: string;
+  value: string;
+}
 const editParams = ref<Param[]>([]);
+const editPaths = ref<PathItem[]>([]);
 const lockedKeyMap = ref<Record<string, boolean>>({});
 
+// 編集したparamsとpathsを結合して新しいURLを作成
 const editedUrl = computed(() => {
   const newUrl = new URL(props.url.href);
   // paramsからsearchを作成
@@ -24,6 +30,12 @@ const editedUrl = computed(() => {
     searchParams.append(param.key, param.value);
   });
   newUrl.search = searchParams.toString();
+  newUrl.pathname = editPaths.value
+    .flatMap((path) => {
+      if (path.value == "") return [];
+      return encodeURIComponent(path.value);
+    })
+    .join("/");
 
   return newUrl;
 });
@@ -41,6 +53,17 @@ function init(url: URL) {
 
   editParams.value = params;
 
+  const paths = url.pathname
+    .split("/")
+    .slice(1)
+    .map((path) => {
+      return { id: crypto.randomUUID(), value: decodeURIComponent(path) };
+    });
+
+  paths.push(createNewPath());
+
+  editPaths.value = paths;
+
   // lockMapの初期化
   lockedKeyMap.value = {};
 }
@@ -49,28 +72,41 @@ function toggleLock(key: string) {
   lockedKeyMap.value[key] = !lockedKeyMap.value[key];
 }
 
-function removeKey(id: string) {
+function removeParam(id: string) {
   editParams.value = editParams.value.filter((param) => param.id !== id);
+}
+function removePath(id: string) {
+  editPaths.value = editPaths.value.filter((path) => path.id !== id);
 }
 function createNewParam() {
   return { id: crypto.randomUUID(), key: "", value: "" };
 }
-function onUpdate() {
-  console.log("onUpdate");
+function createNewPath() {
+  return { id: crypto.randomUUID(), value: "" };
+}
+function onUpdateParam() {
   const lastParam = editParams.value.at(-1);
   if (!lastParam) return;
 
-  console.log("lastParam", lastParam);
   // 最後のエントリが空でない場合は新しいエントリを追加
   if (lastParam.key || lastParam.value) {
     editParams.value.push(createNewParam());
+  }
+}
+function onUpdatePath() {
+  const lastPath = editPaths.value.at(-1);
+  if (!lastPath) return;
+
+  // 最後のエントリが空でない場合は新しいエントリを追加
+  if (lastPath.value) {
+    editPaths.value.push(createNewPath());
   }
 }
 
 function reset() {
   init(props.url);
 }
-function removeAll() {
+function removeAllParams() {
   // ロックされているエントリを残す
   const filteredParams = editParams.value.filter((param) => {
     return lockedKeyMap.value[param.id];
@@ -95,7 +131,7 @@ watch(
 </script>
 
 <template>
-  <div class="flex flex-col p-4 text-sm gap-4">
+  <div class="flex flex-col p-4 text-sm gap-4 mb-16 min-w-[500px]">
     <section>
       <div class="flex flex-row justify-between items-center">
         <h1 class="text-lg font-bold">URL</h1>
@@ -116,62 +152,91 @@ watch(
       />
     </section>
     <section>
-      <h2 class="text-lg font-bold">Pathes</h2>
+      <details>
+        <summary class="text-lg font-bold">Paths</summary>
+        <table>
+          <tbody>
+            <tr v-for="({ id }, i) in editPaths" :key="id">
+              <td class="min-w-8">
+                <button
+                  v-if="i !== editPaths.length - 1"
+                  class="flex bg-white p-1"
+                  @click="removePath(id)"
+                >
+                  <i class="i-mdi-trash-can-outline bg-red-500 w-5 h-5"></i>
+                </button>
+              </td>
+
+              <td class="p-2 w-full">
+                <input
+                  v-model="editPaths[i].value"
+                  class="border p-1 w-full"
+                  :disabled="lockedKeyMap[id]"
+                  placeholder="value"
+                  @input="onUpdatePath"
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </details>
     </section>
 
     <section>
-      <h2 class="text-lg font-bold">Params</h2>
+      <details open>
+        <summary class="text-lg font-bold">Params</summary>
 
-      <table class="mb-16">
-        <tbody>
-          <tr v-for="({ id, key, value }, i) in editParams" :key="id">
-            <td class="min-w-8">
-              <button
-                v-if="i !== editParams.length - 1"
-                class="flex bg-white p-1"
-                @click="removeKey(id)"
-              >
-                <i class="i-mdi-trash-can-outline bg-red-500 w-5 h-5"></i>
-              </button>
-            </td>
+        <table>
+          <tbody>
+            <tr v-for="({ id }, i) in editParams" :key="id">
+              <td class="min-w-8">
+                <button
+                  v-if="i !== editParams.length - 1"
+                  class="flex bg-white p-1"
+                  @click="removeParam(id)"
+                >
+                  <i class="i-mdi-trash-can-outline bg-red-500 w-5 h-5"></i>
+                </button>
+              </td>
 
-            <td class="min-w-8">
-              <button
-                v-if="i !== editParams.length - 1"
-                class="flex bg-white p-1"
-                @click="toggleLock(id)"
-              >
-                <i
-                  :class="`${
-                    lockedKeyMap[id]
-                      ? 'i-mdi-lock bg-black'
-                      : 'i-mdi-lock-open bg-slate-300'
-                  }  w-5 h-5`"
-                ></i>
-              </button>
-            </td>
+              <td class="min-w-8">
+                <button
+                  v-if="i !== editParams.length - 1"
+                  class="flex bg-white p-1"
+                  @click="toggleLock(id)"
+                >
+                  <i
+                    :class="`${
+                      lockedKeyMap[id]
+                        ? 'i-mdi-lock bg-black'
+                        : 'i-mdi-lock-open bg-slate-300'
+                    }  w-5 h-5`"
+                  ></i>
+                </button>
+              </td>
 
-            <td class="p-2">
-              <input
-                v-model="editParams[i].key"
-                class="border p-1"
-                :disabled="lockedKeyMap[key]"
-                placeholder="key"
-                @input="onUpdate"
-              />
-            </td>
-            <td class="p-2">
-              <input
-                v-model="editParams[i].value"
-                class="border p-1"
-                :disabled="lockedKeyMap[key]"
-                placeholder="value"
-                @input="onUpdate"
-              />
-            </td>
-          </tr>
-        </tbody>
-      </table>
+              <td class="p-2">
+                <input
+                  v-model="editParams[i].key"
+                  class="border p-1"
+                  :disabled="lockedKeyMap[id]"
+                  placeholder="key"
+                  @input="onUpdateParam"
+                />
+              </td>
+              <td class="p-2 w-full">
+                <input
+                  v-model="editParams[i].value"
+                  class="border p-1 w-full"
+                  :disabled="lockedKeyMap[id]"
+                  placeholder="value"
+                  @input="onUpdateParam"
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </details>
     </section>
 
     <footer
@@ -180,7 +245,7 @@ watch(
       <!-- remove all button -->
       <button
         class="bg-red-500 text-white py-2 px-2 rounded-md flex items-center gap-2 font-bold"
-        @click="removeAll"
+        @click="removeAllParams"
       >
         <i class="i-mdi-trash-can-outline w-5 h-5"></i>
         remove all params
